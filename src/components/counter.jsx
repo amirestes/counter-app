f54e29540fb48b233a08ce687bbc20d5b8e9a08e
amirestes <amirestes@gmail.com>
2022-06-12 09:15:59 -0500

    updated the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,52 +1,57 @@
 // import { getByPlaceholderText } from '@testing-library/react';
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        value: this.props.value
-        //imageUrl: 'https://picsum.photos/200',
-        //tags: ['tag1', 'tag2', 'tag3']
+  state = {
+    value: this.props.counter.value,
+    //imageUrl: 'https://picsum.photos/200',
+    //tags: ['tag1', 'tag2', 'tag3']
+  };
 
-    };
+  // constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  // }
 
-    // constructor() { 
-    //     super();
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
+  handleIncrement = (product) => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
-    handleIncrement = product => {
-        this.setState({ value: this.state.value + 1 });
-    };
-
-
-    render() { 
-        return (
-            <div>
-                <h4>{this.props.id}</h4>
-                {/* <img src={this.state.imageUrl} alt="" /> */}
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button 
-                onClick={() => this.handleIncrement()} 
-                className="btn btn-secondary btn-sm">Increment</button>
-                {/* <ul>
+  render() {
+    return (
+      <div>
+        <h4>{this.props.id}</h4>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* <ul>
                     { this.state.tags.map(tag => <li key={ tag }>{ tag } </li>) }
                 </ul> */}
-                <button className="btn btn-danger btn-sm m-2" onClick={this.handleDelete}>Delete</button>
-            </div>
-        );
-    }
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
-        return classes;
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-    formatCount() {
-        const { value: count } = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
